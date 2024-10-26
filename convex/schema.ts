baa9e92d6e5cc8401bc2c816804181f6773e0e3e
Salif Tankoano <tanksalif@gmail.com>
2024-10-26 13:30:57 -0400

    add users to the database

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,6 +1,11 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 export default defineSchema({
+  users: defineTable({
+    username: v.string(),
+    image: v.string(),
+    clerkId: v.string(),
+  }).index("by_clerkId", ["clerkId"]),
   messages: defineTable({
     sender: v.string(),
     content: v.string(),
