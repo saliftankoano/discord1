96ab3e8af7f9947165bb329027e05b9c49dee301
Salif Tankoano <tanksalif@gmail.com>
2024-11-06 16:37:59 -0500

    change sidebar to dark

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -5,14 +5,15 @@ import {
 } from "./_components/friends-list";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AddFriend } from "./_components/add-friend";
+
 export default function Page() {
   return (
-    <div className="flex-1 flex-col flex divide-y">
-      <header className="flex items-center justify-between p-4">
+    <div className="flex-1 flex-col flex divide-y page-background">
+      <header className="flex items-center justify-between p-4 page-header">
         <h1 className="font-semibold">Friends</h1>
         <AddFriend />
       </header>
-      <div className="grid p-4 gap-4">
+      <div className="grid p-4 gap-4 page-section">
         <TooltipProvider delayDuration={0}>
           <PendingFriendsList />
           <AcceptedFriendsList />
